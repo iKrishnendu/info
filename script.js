59c46376da7cd5552539e9e47b82160cab057e13
Krishnendu Sahoo <102577456+iKrishnendu@users.noreply.github.com>
2022-06-13 21:27:54 +0530

    my portfolio website

---
+++ script.js
@@ -0,0 +1,14 @@
+// console.log("code running...")
+// document.querySelector('.cross').style.display = 'none';
+// document.querySelector('.hamburger').addEventListener("click",()=>{
+//     document.querySelector('.sideber').classList.toggle('sidebarGo')
+//     if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+//         document.querySelector('.sidebar').style.display = 'inline'
+//         document.querySelector('.sidebar').style.display = 'none'
+//     }
+//     else{
+//         document.querySelector('.sidebar').style.display = 'none'
+//         document.querySelector('.sidebar').style.display = 'inline'
+    
+//     }
+// })
